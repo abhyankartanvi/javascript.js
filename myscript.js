cfc868171ed94873327299d85830aa2f88cc19eb
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-08-15 14:07:22 +1000

    index and js updated

---
+++ myscript.js
@@ -0,0 +1,20 @@
+// click grow to increase the box size
+function myFunction() {
+ document.getElementById("box").style.height = "300px";
+
+}
+// click blue to turn the box blue
+function myFunction1() {
+ document.getElementById("box").style.backgroundColor = "rgb(0, 0, 255)";
+
+}
+// click fade to fade the box
+function myFunction2() {
+ document.getElementById("box").style.backgroundColor = "rgba(255, 165, 0, 0.6)";
+
+}
+//click reset to go back to default
+function myFunction3() {
+ document.getElementById("box").style.height = "150px";
+
+}
